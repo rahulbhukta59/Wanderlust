4a6aaa2e5ae31761b6baf77620966bcc77526798
rahulbhukta59 <your_email_for_rahulbhukta59@domain.com>
2025-10-28 15:42:15 +0530

    Updated

--- app.js
+++ app.js
@@ -83,6 +83,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+  res.locals.currUser = req.user;
+  next();
+});
+
 app.use((req,res,next) =>{
     res.locals.currUser = req.user;
     res.locals.success = req.flash("success");
